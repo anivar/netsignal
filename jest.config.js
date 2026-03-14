614285cec2682d0057da32830e7f9e917d5b4146
anivar <ping@anivar.net>
2026-03-14 16:27:38 +0530

    chore: finalize build config, add lockfile

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFiles: ['./__tests__/setup.ts'],
-  testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+  testMatch: ['<rootDir>/__tests__/**/*.test.(ts|tsx)'],
+  testPathIgnorePatterns: ['/node_modules/', '/example/'],
 };
